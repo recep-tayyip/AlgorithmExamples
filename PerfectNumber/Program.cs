12edbce7489d03ca6cd547cb9b8b2eeb0a2c4ae9
Recep Tayyip Sever <2103008058@ogrenci.sbu.edu.tr>
2022-04-14 23:19:19 +0300

    -

---
+++ PerfectNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("İnput value= ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            PerfectNumber(number);
+        }
+
+        static void PerfectNumber(int number)
+        {
+            int total = 0;
+            for (int i = 1; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                    total += i;
+            }
+
+            if (number == total)
+            {
+                Console.Write($"{number} is a perfect number");
+            }
+            else
+            {
+                Console.Write($"{number} is not a perfect number");
+            }
+
+        }
+    }
+}
---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number= ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            PrimeNumber(number);
+        }
+
+        static void PrimeNumber(int number)
+        {
+            bool isPrime = true;
+            if (number==0 || number==1)
+            {
+                Console.Write("Prime numbers start from 2");
+            }
+            else
+            {
+                for (int i = 2; i < number; i++)
+                {
+                    if (number%i==0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write($"{number} is a prime number");
+                }
+                else
+                {
+                    Console.Write($"{number} is not a prime number");
+                }
+            }
+        }
+    }
+}
