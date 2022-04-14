6a7892272b51ad62dc52e0c6aca8887803786de7
Recep Tayyip Sever <2103008058@ogrenci.sbu.edu.tr>
2022-04-14 22:36:24 +0300

    Add project files.

---
+++ ExponentialFonksiyon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExponentialFonksiyon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give a base value= ");
+            int baseNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Give a exponent value= ");
+            int expoNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"{baseNumber} ^ {expoNumber} = {Exponant(baseNumber,expoNumber)}");
+        }
+
+        static int Exponant(int number, int expo)
+        {
+            if (expo==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Exponant(number, expo - 1);
+            }
+        }
+    }
+}
---
+++ FaktorialCalculation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FaktorialCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number=");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"{number}! = {Faktorial(number)}");
+        }
+
+        static int Faktorial(int number)
+        {
+            if (number==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Faktorial(number - 1);
+            }
+        }
+    }
+}
