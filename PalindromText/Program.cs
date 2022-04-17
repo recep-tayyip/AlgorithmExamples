cbdec1daad5bfbfdc9a86e96a17178a57e129082
Recep Tayyip Sever <2103008058@ogrenci.sbu.edu.tr>
2022-04-17 23:39:42 +0300

    -

---
+++ PalindromText/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PalindromText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("text:");
+            string text = Console.ReadLine();
+            string text_pal = "";
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                text_pal += text[i];
+            }
+
+            Console.WriteLine($"reverse text:{text_pal}");
+            if (text_pal == text)
+            {
+                Console.WriteLine($"{text} is palindrom");
+            }
+            else
+            {
+                Console.WriteLine($"{text} is not palindrom");
+            }
+        }
+    }
+}
